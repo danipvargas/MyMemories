8f313dabeafa5945508e57b57e09631e0a7bfdc6
DaniPVargas <danpv2000@gmail.com>
2026-07-31 14:42:57 +0200

    Separated files for DB models

---
+++ backend/src/models/postcard.py
@@ -0,0 +1,43 @@
+from datetime import date
+from enum import Enum
+
+from geoalchemy2 import Geometry
+from sqlalchemy import Date, Integer, String, Text
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class DatePrecision(Enum):
+    DAY = "day"
+    MONTH = "month"
+    YEAR = "year"
+    UNKNOWN = "unknown"
+
+
+class PostCard(Base):
+    __tablename__ = "postcards"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    image_path: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    adquisition_date: Mapped[date] = mapped_column(Date, nullable=False)
+
+    adquisition_date_precision: Mapped[DatePrecision] = mapped_column(
+        SQLEnum(DatePrecision), nullable=False
+    )
+
+    country: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    city: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    region: Mapped[str] = mapped_column(String(50))
+
+    coordinates: Mapped[object] = mapped_column(
+        Geometry("POINT", srid=4326),
+        nullable=False,
+    )
+
+    description: Mapped[str] = mapped_column(Text)
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,19 +1,7 @@
-from datetime import date
-from enum import Enum
-
-from geoalchemy2 import Geometry
-from sqlalchemy import Date, Integer, String, Text
-from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
-
-
-class DatePrecision(Enum):
-    DAY = "day"
-    MONTH = "month"
-    YEAR = "year"
-    UNKNOWN = "unknown"
 
 
 class User(Base):
@@ -26,30 +14,3 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
 
     password_hash: Mapped[str] = mapped_column(Text, nullable=False)
-
-
-class PostCard(Base):
-    __tablename__ = "postcards"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-
-    image_path: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-
-    adquisition_date: Mapped[date] = mapped_column(Date, nullable=False)
-
-    adquisition_date_precision: Mapped[DatePrecision] = mapped_column(
-        SQLEnum(DatePrecision), nullable=False
-    )
-
-    country: Mapped[str] = mapped_column(String(50), nullable=False)
-
-    city: Mapped[str] = mapped_column(String(50), nullable=False)
-
-    region: Mapped[str] = mapped_column(String(50))
-
-    coordinates: Mapped[object] = mapped_column(
-        Geometry("POINT", srid=4326),
-        nullable=False,
-    )
-
-    description: Mapped[str] = mapped_column(Text)
